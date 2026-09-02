3fce8f6956a840911f7d2dd630c7caac7a1c95a1
Erick <erick.ccastillo54@gmail.com>
2026-09-01 21:52:35 -0600

    NotFound

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home';
+import NotFound from './pages/NotFound';
 
 const App: React.FC = () => {
   return (
     <Routes>
       <Route path="/" element={<Home />} />
+
+      <Route path="*" element={<NotFound />} />
     </Routes>
+
+
+
   );
 };
 
